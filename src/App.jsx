045e435ff1340309e8c41708757fd60cc5ac11d8
Gavin Cartier <48884797+18Planet@users.noreply.github.com>
2025-06-23 12:55:14 +0200

    IconListEntry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import IconListEntry from "./components/IconListEntry/IconListEntry";
+
 function App() {
   return (
     <div>
-      App
+      <IconListEntry 
+        text='Ahorra tiempo optimizando la facturación de tu negocio'
+        subtext="La automatización de tus facturas te permitirá reducir el tiempo
+        que dedicas a tu facturación y emplearlo para mejorar tu negocio"
+      
+      />
     </div>
   );
 }
