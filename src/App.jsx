96468b7b2ceaa8b7beef824f4deab97e161ae574
Gavin Cartier <48884797+18Planet@users.noreply.github.com>
2025-06-25 10:25:27 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header.jsx"; 
 import Footer from "./components/footer.jsx"; 
-import IconListEntry from "./components/IconListEntry/IconListEntry";
+import IconListPage from "./pages/IconListPage/IconListPage";
 import FeatureGrid from "./components/FeatureGrid/featureGrid.jsx";
 import MainPage from "./components/MainPage/MainPage.jsx";
 
@@ -10,10 +10,8 @@ function App() {
     <div>
       <Header/>
       <MainPage/>
-      <IconListEntry 
-        text='Ahorra tiempo optimizando la facturación de tu negocio'
-        subtext="La automatización de tus facturas te permitirá reducir el tiempo
-        que dedicas a tu facturación y emplearlo para mejorar tu negocio"
+      <IconListPage 
+        title="Example Icon List Page"
       
       />
 
